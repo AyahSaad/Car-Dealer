8bc178cff0d5f735da973209bcad1a2d5ff89ed9
Your Name <you@example.com>
2023-12-21 12:34:21 +0300

    Advance Car Dealer

---
+++ app/src/main/java/edu/birzeit/advancecardealer/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.birzeit.advancecardealer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
